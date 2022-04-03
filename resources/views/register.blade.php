1684f798e3808646a573133d18a6149281614330
Rishabh Chaurasiya <chaurasiarishabh2@gmail.com>
2022-04-03 17:40:23 +0530

    Added validation in register page

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -7,15 +7,15 @@
         @csrf
         <div class="form-group">
     <label for="exampleInputEmail1">User Name</label>
-    <input type="text" name="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="User Name" >
+    <input type="text" name="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="User Name" required>
   </div>
   <div class="form-group">
     <label for="exampleInputEmail1">Email address</label>
-    <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email" >
+    <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email" required>
   </div>
   <div class="form-group">
     <label for="exampleInputPassword1">Password</label>
-    <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="password" >
+    <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="password" required>
   </div>
   <div class="form-group form-check">
   </div>
